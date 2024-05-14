2763c6e80b4ef5eb9f71533d6882e9a16c0ad9b4
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2024-05-14 18:40:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,13 @@ if (!fs.existsSync(logDir)) {
 }
 
 // Use the cors middleware to allow all CORS requests
-app.use(cors());
-// app.use(cors(
-//   {
-//     origin: ["https://deploy-quality-log-control-api.app"],
-//     methods: ["GET"],
-//     credentials: true
-//   }
-// ));
+app.use(cors(
+  {
+    origin: ["https://deploy-quality-log-control-api.app"],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+));
 
 app.get("/", (req, res) => {
   res.send("Hello! Server is Up")
